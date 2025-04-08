d8089aa057866ef46348d78e1822fa40152463a9
Jatin Gumber <93000878+Jatin-gumber12@users.noreply.github.com>
2025-04-08 18:39:32 +0530

    Updated

--- server.cpp
+++ server.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <fstream>
-#include "httplib.h"      // HTTP server library
-#include "json.hpp"       // JSON library
-#include "GraphFunctions.h"  // Custom graph functions
+#include "httplib.h"
+#include "json.hpp"
+#include "GraphFunctions.h"
 
 using namespace httplib;
-using json = nlohmann::json;  // Alias for JSON
+using json = nlohmann::json;
 
 int main() {
     Server svr;
 
-    // Serve HTML, CSS, and JS files
     svr.Get("/", [](const Request&, Response& res) {
-        std::ifstream file("index.html");
+        std::ifstream file("home.html");
         if (file) {
             std::stringstream buffer;
             buffer << file.rdbuf();
@@ -23,7 +22,43 @@ int main() {
         }
     });
 
-    // Handle route finding API request
+    svr.Get("/index.html", [](const Request&, Response& res) {
+        std::ifstream file("index.html");  // Make sure index.html is in the same folder
+        if (file) {
+            std::stringstream buffer;
+            buffer << file.rdbuf();
+            res.set_content(buffer.str(), "text/html");
+        } else {
+            res.status = 404;
+            res.set_content("File not found: index.html", "text/plain");
+        }
+    });
+
+
+    svr.Get(R"(/(.*))", [](const Request& req, Response& res) {
+        std::string file_path = req.path.substr(1); // remove leading slash
+        std::ifstream file(file_path, std::ios::binary);
+    
+        if (file) {
+            std::stringstream buffer;
+            buffer << file.rdbuf();
+     
+            // Basic content-type detection
+            std::string ext = file_path.substr(file_path.find_last_of('.') + 1);
+            std::string mime = "text/plain";
+            if (ext == "html") mime = "text/html";
+            else if (ext == "css") mime = "text/css";
+            else if (ext == "js") mime = "application/javascript";
+            else if (ext == "webm") mime = "video/webm";
+            else if (ext == "mp4") mime = "video/mp4";
+    
+            res.set_content(buffer.str(), mime);
+        } else {
+            res.status = 404;
+            res.set_content("File not found: " + file_path, "text/plain");
+        }
+    });
+    
     svr.Post("/find-route", [](const Request& req, Response& res) {
         try {
             json request_json = json::parse(req.body);
@@ -66,7 +101,6 @@ int main() {
         }
     });
 
-    // Serve the generated output.html
     svr.Get("/output.html", [](const Request&, Response& res) {
         std::ifstream file("output.html");
         if (file) {
